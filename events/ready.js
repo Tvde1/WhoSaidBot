a74ee7bb49e7efc791811a08c88353db6bb68a7b
Tim van den Essen <tim3345@live.nl>
2017-10-28 02:34:38 +0200

    oops

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,11 @@
-const ExtendedClient = require('../extendedClient');
+const ExtendedClient = require('../extendedClient'); //eslint-disable-line no-unused-vars
 
 /**
  * @param {ExtendedClient} client 
  */
 function run (client) {
     client.logger.log('Ready', 'Bot is ready.');
-    client.user.setPresence({ game: { name: 'ws!help' });
+    client.user.setPresence({ game: { name: 'ws!help' }});
 }
 
 module.exports = run;
